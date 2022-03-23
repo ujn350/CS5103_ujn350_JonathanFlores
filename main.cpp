6ccedd712684277892c9c4f987f74014a4dc4066
JonathanGLaptop <jonathan.flores2@my.utsa.edu>
2022-03-23 14:03:16 -0500

    Tuesday, March 22nd school work on replace branch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iterator>
 #include <fstream>
 #include <sstream>
 #include "word.h"
@@ -92,27 +93,102 @@ void display_word_frequency(string &text)
 
         }
     }
+    cout << "------------------------- WORD FREQUENCIES -------------------------" << endl;
     for(int i = 0; i < word_history.size(); i++)
     {
-        cout << "-----------------------------------------------------------" << endl;
-        cout << "word: " << "\"" << word_history[i].get_wordname() << "\"" << endl;
-        cout << "freq: " << word_history[i].get_instances() << endl;
+        cout << "\"" << word_history[i].get_wordname() << "\": " << word_history[i].get_instances() << endl;
     }
-    cout << "-----------------------------------------------------------" << endl << endl;
+    cout << "--------------------------------------------------------------------" << endl << endl;
 
+}
+
+bool replace(string &text)
+{
+    string targetword;
+    string replaceword;
+
+    cout << "------------------------- WORD REPLACEMENT -------------------------" << endl;
+    cout << "Please enter the target word to replace: " << endl;
+    cin >> targetword;
+    cout << "Enter what to replace target word with: " << endl;
+    cin >> replaceword;
+
+    string::iterator head = text.begin();
+    string::iterator tail = head;
+    string::iterator const end = text.end();
+
+    string testword;
+
+    while(head != end && tail != end)
+    {
+        if(!IsSeparator(*tail))
+        {
+            tail++;
+        }
+        else if(distance(head, tail - 1) == targetword.length())
+        {
+            testword.clear();
+            for(int i = 0; i < targetword.length(); i++)
+            {
+                testword.push_back(*(head + i));
+            }
+            if(testword == targetword)
+            {
+                if(replaceword.length() == targetword.length())
+                {
+
+                }
+                else if(replaceword.length() > targetword.length())
+                {
+
+                }
+                else if(replaceword.length() < targetword.length())
+                {
+
+                }
+            }
+        }
+
+    }
 }
 
 int main(){
     cout << endl << "############################# STRINGS AND WORDS #############################" << endl << endl;
-    cout << "reading text file contents..." << endl;
+    cout << "reading text file contents..." << endl << endl;
 
-    //grabs text from stream and creates string of full text
     ifstream input("input_text.txt", ifstream::in);
     stringstream inputStream;
     inputStream << input.rdbuf();
     string text = inputStream.str();
 
-    display_word_frequency(text);
+    int selection;
 
+    while(1)
+    {
+        cout << "What would you like to perform? Enter the integer associated with your selection." << endl;
+        cout << "1: Display Word Frequency" << endl;
+        cout << "2: Replace Words" << endl;
+        cout << "9: Quit" << endl;
+        cin >> selection;
+
+        if(selection == 1)
+        {
+            display_word_frequency(text);
+        }
+        else if(selection == 2)
+        {
+            replace(text);
+        }
+        else if(selection == 9)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Not a valid selection." << endl;
+        }
+    }
+
+    cout << "Good bye! :)" << endl;
     return 0;
 }
